85b2b48a27a119fcc8e04144dcaf90463fc81498
Paula <paula.rodriguez.balseiro@gmail.com>
2020-12-04 20:47:39 +0100

    first commit

---
+++ precache-manifest.51e25fe497acc8a9bdd8fa7aa6b9e974.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "33ed1b31f0450225947431967d38aad2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "224a3bb95aecd9bacd58",
+    "url": "/static/css/2.3c25ef27.chunk.css"
+  },
+  {
+    "revision": "4e61bbcdc383b860ddfc",
+    "url": "/static/css/main.9492c2a2.chunk.css"
+  },
+  {
+    "revision": "224a3bb95aecd9bacd58",
+    "url": "/static/js/2.778bdc06.chunk.js"
+  },
+  {
+    "revision": "57af66c67ce7743363190356615b253d",
+    "url": "/static/js/2.778bdc06.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4e61bbcdc383b860ddfc",
+    "url": "/static/js/main.04636d3b.chunk.js"
+  },
+  {
+    "revision": "f5230ca0bfe38b95074e",
+    "url": "/static/js/runtime-main.14257d7f.js"
+  },
+  {
+    "revision": "48885972103391be19857cbd32302db4",
+    "url": "/static/media/ocio_asturias.48885972.jpg"
+  }
+]);
